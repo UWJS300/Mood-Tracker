eafc6d0625c35791c2313c03bf0dcacd59402c67
James Trimarco <james.trimarco@gmail.com>
2017-06-06 14:36:52 -0700

    Removed default React logo and replaced with Mood Tracker

---
+++ my-app/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>
+        <span className='car-word'>Mood Tracker</span>
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
