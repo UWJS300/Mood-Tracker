a360aabba1819c6ba21b37e8939a166bd907fe9a
James Trimarco <james.trimarco@gmail.com>
2017-06-09 13:38:53 -0700

    Breaking out Chrome extension into separate folder

---
+++ my-app/extension/README.md
@@ -0,0 +1,16 @@
+# JS300 Project: Mindful Reader
+
+## Summary
+
+This app is a browser extension that allows a reader to to track their mood while browsing the internet. A reader can see the data they have tracked over time.
+
+## Tools
+- React - create-react-app
+- Firebase
+- Chrome extension API
+- [ ] Authentication (maybe [Google sign-in](https://developers.google.com/identity/sign-in/web/sign-in) since it's a Chrome extension?)
+- [ ] CSS framework TBD
+
+### [(Project spec with user stories](https://docs.google.com/document/d/1RqHuXOvQd-_c24zJDJVuEs1HSwskmIC1JnUg147Q6qI/edit?usp=sharing)
+
+### [Wireframes](https://xd.adobe.com/view/7cbf8329-a602-4b63-9de6-bb557a6bbd42/screen/4ab492e9-43ae-4a20-bc36-2b24111f613e/main-article)
---
+++ my-app/extension/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import Article from './Article';
+import History from './History';
+import Settings from './Settings';
+import Login from './Login';
+
+class App extends Component {
+  render() {
+    const userLoggedIn = true; //this needs to be addressed with state
+
+    if (userLoggedIn) {
+      return (
+        <div>
+        <div className="tabHeader">
+          <h2>Mindful Reader: Chrome Extension Version</h2>
+        </div>
+        <div className="currentMood">
+      <h3>Current mood</h3>
+        <Article />
+      </div>
+
+	  <div className="moodHistory">
+      <h3>Mood history</h3>
+        <History />
+      </div>
+
+	<div className="moodSettings">
+      <h3>Mood settings</h3>
+        <Settings />
+      </div>
+        </div>
+        );
+    } else {
+      return (
+        <div>
+          <Login />
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
