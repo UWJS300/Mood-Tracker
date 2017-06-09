ede90642d0182ac91009d9dfebe78495a30276f6
James Trimarco <james.trimarco@gmail.com>
2017-06-09 13:38:53 -0700

    Breaking out Chrome extension into separate folder

---
+++ my-app/extension/src/History.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+class History extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {selectedOption: 'mood1', submittedMood: '', comment: '', URL: '', timeStamp: ''};
+      this.handleOptionChange = this.handleOptionChange.bind(this);
+    }
+  handleOptionChange(changeEvent) {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  }
+  handleFormSubmit(formSubmitEvent) {
+    formSubmitEvent.preventDefault()
+  
+  }
+  render () {
+    return (
+
+		<div className="moodGraph">
+	<div className="graphThumb">Graph goes here
+		</div>
+		<button id="viewHistory">See more history</button>
+				
+</div>
+		
+
+
+
+
+    );
+  
+	  
+  }
+};
+
+export default History;
